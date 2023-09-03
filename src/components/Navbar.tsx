68fb1321b3e7fbdaed4784ba54030057eb5de504
vaibhav <vaibhavsahni2001@gmail.com>
2023-09-03 17:09:25 +0530

    added prompt

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,13 +3,19 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import Link from "next/link";
 import { Badge } from "./ui/badge";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <header className="mt-5">
       <nav className="flex justify-between">
         <section className="flex items-start gap-2">
-          <img src="/storygen-logo.webp" className="h-16 w-40" />
+          <Image
+            src="/storygen-logo.webp"
+            alt="storygen"
+            height={64}
+            width={160}
+          />
           <Badge variant="beta">Beta</Badge>
         </section>
         <section className="flex gap-7 z-10">
